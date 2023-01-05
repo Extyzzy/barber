ba1d1f17493d74ec366c4885b990f2a3b5380ace
Valentin Leanca <valentin@offerte.nl>
2023-01-05 20:47:06 +0200

    up flags

--- pages/components/Layout/Header/ChangeLanguageComponent.tsx
+++ pages/components/Layout/Header/ChangeLanguageComponent.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import Russia from "../../../../public/img/flags/russia.svg";
+import Romania from "../../../../public/img/flags/romania.svg";
 
 export default function ChangeLanguageComponent() {
   const { i18n } = useTranslation();
@@ -12,8 +15,19 @@ export default function ChangeLanguageComponent() {
   }
 
   return (
-    <div className="cursor-pointer text-xs sm:text-lg z-10 text-white" onClick={() => changeLanguageFn()}>
-      RO / RU
+    <div className="cursor-pointer text-xs sm:text-lg z-10 text-white flex items-center bg-white rounded-xl px-2" onClick={() => changeLanguageFn()}>
+
+      {
+        (i18n.language === 'ru' &&(
+            <>
+              <Image width={30}  height={30} src={Russia} alt="mr-2" /> <div className='ml-2 text-md text-black'>RU</div>
+            </>
+          )) || (
+       <>
+         <Image width={30} height={30} src={Romania} alt="mr-2" className='block' /> <div className='ml-2 text-md text-black'>RO</div>
+       </>
+        )
+      }
     </div>
   )
 }
