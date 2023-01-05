f8bde739a855cdcdfca459d46f9421b5f72b2748
Valentin Leanca <valentin@offerte.nl>
2023-01-05 19:38:56 +0200

    up home

--- pages/components/MetaTags.tsx
+++ pages/components/MetaTags.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function MetaTags() {
   return (
-    <title>Select Barber</title>
+    <title>SELECT BARBER</title>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
       <div className='flex items-center justify-center h-[80vh]'>
           <div className={`h-full flex justify-center sm:items-center items-end relative text-center ${i18n.language === 'ru' ? 'tracking-[0.3em]' : ''}`}>
-              <div className='sm:mb-0 mb-20'>
+              <div className='sm:mb-0 mb-20 pb-4'>
                   <h1 className={`sm:text-6xl text-3xl tracking-[0.3rem] ${i18n.language === 'ru' ? 'marks' : 'allura'}  `}>{t('general.main_text')}</h1>
                   <div className="text-center mt-4">
                       <ButtonLink target='_blank' to='https://mst.link/selectbarbershop' text={t('general.schedule')} />
