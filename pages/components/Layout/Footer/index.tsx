c623fe4554a46df8a9cc708bd0c6d654f2c18656
Leanca Valentin <>
2024-11-28 21:35:11 +0200

    add loc

--- pages/components/Layout/Footer/index.tsx
+++ pages/components/Layout/Footer/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Phone from '../../../../public/img/svg/phone-call.svg';
 import {useRouter} from "next/router";
 import {useTranslation} from "react-i18next";
+import Location from "../../../../public/img/svg/locationBlack.svg";
 
 export default function Footer() {
   const route = useRouter();
@@ -27,26 +28,35 @@ export default function Footer() {
           {
               !hideFooter && (
                   <div className="flex justify-center text-center">
-                      <div className="absolute bottom-0 bg-zinc-800/75 p-5 w-full">
+                      <div className="absolute bottom-0 bg-zinc-800/75 p-2 sm:p-5 w-full">
                           <div className='hidden sm:block sm:font-bold text-xl'>
                               +373 79 000 740
                           </div>
                           <div className='flex flex-wrap lg:flex-nowrap justify-center lg:gap-16 text-center sm:w-[60%] mx-auto'>
-                              <h3 className="mt-2 sm:mt-4  order-2 sm:order-1 font-bold text-xl w-[16rem] mx-auto">
+                            
+                            <button className="order-2 sm:order-1 bg-white text-black rounded-2xl mt-2 sm:mt-4 p-2">
+                                <h3 className="font-bold text-base sm:text-xl w-[16rem] mx-auto">
                                 <a target="_blank" href="https://www.google.com/maps/place/SELECT+BARBER/@47.0238106,28.8019495,14z/data=!4m10!1m2!2m1!1sSELECT+BARBERSHOP+103!3m6!1s0x40c97da5fc964ea1:0xc438e7f992849ddd!8m2!3d47.0238106!4d28.8400583!15sChVTRUxFQ1QgQkFSQkVSU0hPUCAxMDNaFyIVc2VsZWN0IGJhcmJlcnNob3AgMTAzkgELYmFyYmVyX3Nob3CaASRDaGREU1VoTk1HOW5TMFZKUTBGblNVUkllazFmV1Y5QlJSQULgAQD6AQQIIhA1!16s%2Fg%2F11vj3zxhhy!5m1!1e1?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D">
-                                str. Columna 103
+                                <Image width={25} src={Location} alt="str. Columna 33"/> str. Columna 103
                                 </a>
                               </h3>
-                              <h3 className="order-1 sm:order-2 font-bold text-xl sm:mt-4 sm:w-[28rem] mx-auto">
-                                <a target="_blank" href="https://www.google.com/maps/place/SELECT+BARBER/@47.0292329,28.8307769,17z/data=!3m1!4b1!4m6!3m5!1s0x40c97d00165f3583:0x1f1ad9cd66c06c15!8m2!3d47.0292293!4d28.8356424!16s%2Fg%2F11vx4f_4vk!5m1!1e1?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D">
-                               str. Mitropolit Gavriil Bănulescu-Bodoni 57
-                                </a>
-                              </h3>
-                              <h3 className="order-3 sm:order-3 mt-2 sm:mt-4 font-bold text-xl  w-[16rem] mx-auto">
-                                <a target="_blank" href="https://www.google.com/maps/place/SELECT+BARBERSHOP/@47.0545049,28.8870013,17z/data=!3m1!4b1!4m6!3m5!1s0x40c97d55a1adb603:0x5d6217e0e97b2e5d!8m2!3d47.0545013!4d28.8895816!16s%2Fg%2F11r7n2ybl9!5m1!1e1?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D">
-                                Bd. Mircea cel Bătrân 33
-                                </a>
-                              </h3>
+                            </button>
+                              
+                            <button className="order-1 sm:order-2 bg-white text-black rounded-2xl sm:mt-4 mt-2 p-2">
+                                <h3 className="font-bold text-base sm:text-xl sm:w-[29rem] mx-auto">
+                                    <a target="_blank" href="https://www.google.com/maps/place/SELECT+BARBER/@47.0292329,28.8307769,17z/data=!3m1!4b1!4m6!3m5!1s0x40c97d00165f3583:0x1f1ad9cd66c06c15!8m2!3d47.0292293!4d28.8356424!16s%2Fg%2F11vx4f_4vk!5m1!1e1?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D">
+                                <Image width={25} src={Location} alt="str. Mitropolit Gavriil Bănulescu-Bodoni 57"/>  str. Mitropolit Gavriil Bănulescu-Bodoni 57
+                                    </a>
+                                </h3>
+                            </button>
+
+                               <button className="order-3 sm:order-3 bg-white text-black rounded-2xl mt-2 sm:mt-4 py-2 px-2">
+                                <h3 className="font-bold text-base sm:text-xl  w-[18rem] mx-auto">
+                                    <a target="_blank" href="https://www.google.com/maps/place/SELECT+BARBERSHOP/@47.0545049,28.8870013,17z/data=!3m1!4b1!4m6!3m5!1s0x40c97d55a1adb603:0x5d6217e0e97b2e5d!8m2!3d47.0545013!4d28.8895816!16s%2Fg%2F11r7n2ybl9!5m1!1e1?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D">
+                                <Image width={25} src={Location} alt="Bd. Mircea cel Bătrân 33"/> Bd. Mircea cel Bătrân 33
+                                    </a>
+                                </h3>
+                              </button>
                           </div>
 
                       </div>
