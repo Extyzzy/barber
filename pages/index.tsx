0216914dea8f6e8668c3f089bc6637410cda0715
Leanca Valentin <>
2024-11-28 20:47:44 +0200

    small fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
       <div className='flex items-center justify-center h-[80vh]'>
           <div className={`h-full flex justify-center sm:items-center items-end relative text-center ${i18n.language === 'ru' ? 'tracking-[0.3em]' : ''}`}>
-              <div className='sm:mb-0 mb-20 pb-0'>
+              <div className='sm:mb-0 mb-60 pb-0'>
                   <h1 className='hidden'>Barber</h1>
                   <h2 className={`sm:text-6xl text-3xl tracking-[0.3rem] ${i18n.language === 'ru' ? 'marks' : 'allura'}  `}>{t('general.main_text')}</h2>
                   <div className="text-center mt-4">
